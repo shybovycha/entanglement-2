5c07848f7b71c6ceb43c55dd4b54cfa61732f875
Artem Shubovych <shybovycha@gmail.com>
2017-03-06 20:23:06 +0100

    Initial Commit

---
+++ entanglement-2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  entanglement-2
+//
+//  Created by Artem Shubovych on 06/03/2017.
+//  Copyright © 2017 Artem Shubovych. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
